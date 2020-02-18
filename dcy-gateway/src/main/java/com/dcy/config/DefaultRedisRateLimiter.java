61cd0e8dca2dc14f7492e7f66309f27e0d62ca9b
dcy <1540770111@qq.com>
2020-02-18 11:25:53 +0800

    添加网关降级和限流

--- dcy-gateway/src/main/java/com/dcy/config/DefaultRedisRateLimiter.java
+++ dcy-gateway/src/main/java/com/dcy/config/DefaultRedisRateLimiter.java
@@ -1,35 +1,35 @@
-//package com.dcy.config;
-//
-//import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.data.redis.core.ReactiveRedisTemplate;
-//import org.springframework.data.redis.core.script.RedisScript;
-//import org.springframework.validation.Validator;
-//import reactor.core.publisher.Mono;
-//
-//import java.util.List;
-//
-///**
-// * @Author：dcy
-// * @Description:
-// * @Date: 2020-02-16 13:54
-// */
-//@Configuration
-//public class DefaultRedisRateLimiter extends RedisRateLimiter {
-//
-//    Config getDefaultConfig() {
-//        return super.getConfig().get("defaultFilters");
-//    }
-//
-//    public DefaultRedisRateLimiter(ReactiveRedisTemplate<String, String> redisTemplate, RedisScript<List<Long>> script, Validator validator) {
-//        super(redisTemplate, script, validator);
-//    }
-//
-//    @Override
-//    public Mono<Response> isAllowed(String routeId, String id) {
-//        if (null == super.getConfig().get(routeId))
-//            getConfig().put(routeId, getDefaultConfig());
-//        return super.isAllowed(routeId, id);
-//    }
-//}
-//
+package com.dcy.config;
+
+import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
+import org.springframework.data.redis.core.script.RedisScript;
+import org.springframework.validation.Validator;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+/**
+ * @Author：dcy
+ * @Description:
+ * @Date: 2020-02-16 13:54
+ */
+@Configuration
+public class DefaultRedisRateLimiter extends RedisRateLimiter {
+
+    Config getDefaultConfig() {
+        return super.getConfig().get("defaultFilters");
+    }
+
+    public DefaultRedisRateLimiter(ReactiveRedisTemplate<String, String> redisTemplate, RedisScript<List<Long>> script, Validator validator) {
+        super(redisTemplate, script, validator);
+    }
+
+    @Override
+    public Mono<Response> isAllowed(String routeId, String id) {
+        if (null == super.getConfig().get(routeId))
+            getConfig().put(routeId, getDefaultConfig());
+        return super.isAllowed(routeId, id);
+    }
+}
+
---
+++ dcy-gateway/src/main/java/com/dcy/config/GatewayFallbackConfig.java
@@ -0,0 +1,29 @@
+package com.dcy.config;
+
+import com.dcy.handler.HystrixFallbackHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.server.RequestPredicates;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+
+/**
+ * @Author：dcy
+ * @Description:
+ * @Date: 2020-02-18 11:20
+ */
+@Configuration
+public class GatewayFallbackConfig {
+    @Autowired
+    private HystrixFallbackHandler hystrixFallbackHandler;
+
+    @Bean
+    public RouterFunction routerFunction() {
+        return RouterFunctions.route(
+                RequestPredicates.GET("/defaultfallback")
+                        .and(RequestPredicates.accept(MediaType.TEXT_PLAIN)), hystrixFallbackHandler);
+    }
+
+}
