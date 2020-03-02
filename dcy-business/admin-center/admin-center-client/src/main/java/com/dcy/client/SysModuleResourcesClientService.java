3c5ae65623a94136908a3f8bc0b8b767fc702a87
dcy <1540770111@qq.com>
2020-03-02 14:17:55 +0800

    优化代码

--- dcy-business/admin-center/admin-center-client/src/main/java/com/dcy/client/SysModuleResourcesClientService.java
+++ dcy-business/admin-center/admin-center-client/src/main/java/com/dcy/client/SysModuleResourcesClientService.java
@@ -2,15 +2,27 @@ package com.dcy.client;
 
 import com.dcy.api.InterfaceService;
 import com.dcy.api.service.SysModuleResourcesRemoteService;
-import com.dcy.client.fallback.SysModuleResourcesClientServiceFallback;
+import com.dcy.common.model.ResponseData;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * @Author：dcy
- * @Description:
+ * @Description: 用户表暴露远程接口+熔断
  * @Date: 2020/3/2 13:27
  */
-@FeignClient(name = InterfaceService.SERVICE_NAME, fallback = SysModuleResourcesClientServiceFallback.class)
+@FeignClient(name = InterfaceService.SERVICE_NAME, fallback = SysModuleResourcesClientService.SysModuleResourcesClientServiceFallback.class)
 public interface SysModuleResourcesClientService extends SysModuleResourcesRemoteService {
 
+    @Component
+    class SysModuleResourcesClientServiceFallback implements SysModuleResourcesClientService{
+
+        @Override
+        public ResponseData<List<Map<String, Object>>> getModuleByUserId(String userId) {
+            return ResponseData.error("提供者出错了！");
+        }
+    }
 }
--- dcy-business/admin-center/admin-center-client/src/main/java/com/dcy/client/SysUserInfoClientService.java
+++ dcy-business/admin-center/admin-center-client/src/main/java/com/dcy/client/SysUserInfoClientService.java
@@ -1,16 +1,34 @@
 package com.dcy.client;
 
 import com.dcy.api.InterfaceService;
+import com.dcy.api.model.SysUserInfo;
 import com.dcy.api.service.SysUserInfoRemoteService;
-import com.dcy.client.fallback.SysUserInfoClientServiceFallback;
+import com.dcy.common.model.ResponseData;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+
+import java.util.Set;
 
 /**
  * @Author：dcy
- * @Description:
+ * @Description: 用户表暴露远程接口+熔断
  * @Date: 2020/3/2 13:26
  */
-@FeignClient(name = InterfaceService.SERVICE_NAME, fallback = SysUserInfoClientServiceFallback.class)
+@FeignClient(name = InterfaceService.SERVICE_NAME, fallback = SysUserInfoClientService.SysUserInfoServiceFallback.class)
 public interface SysUserInfoClientService extends SysUserInfoRemoteService {
 
+    @Component
+    class SysUserInfoServiceFallback implements SysUserInfoClientService {
+
+
+        @Override
+        public ResponseData<SysUserInfo> getUserInfoByUsername(String username) {
+            return ResponseData.error("提供者出错了！");
+        }
+
+        @Override
+        public ResponseData<Set<String>> getAllPermissionByUserId(String userId) {
+            return ResponseData.error("提供者出错了！");
+        }
+    }
 }
