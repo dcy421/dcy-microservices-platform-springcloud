ef435441c093858b4d21bae133b5377a04ec651a
dcy <1540770111@qq.com>
2020-03-02 16:38:48 +0800

    修改熔断

--- dcy-business/admin-center/admin-center-client/src/main/java/com/dcy/client/SysModuleResourcesClientService.java
+++ dcy-business/admin-center/admin-center-client/src/main/java/com/dcy/client/SysModuleResourcesClientService.java
@@ -3,6 +3,7 @@ package com.dcy.client;
 import com.dcy.api.InterfaceService;
 import com.dcy.api.service.SysModuleResourcesRemoteService;
 import com.dcy.common.model.ResponseData;
+import feign.hystrix.FallbackFactory;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
 
@@ -14,15 +15,20 @@ import java.util.Map;
  * @Description: 用户表暴露远程接口+熔断
  * @Date: 2020/3/2 13:27
  */
-@FeignClient(name = InterfaceService.SERVICE_NAME, fallback = SysModuleResourcesClientService.SysModuleResourcesClientServiceFallback.class)
+@FeignClient(name = InterfaceService.SERVICE_NAME, fallbackFactory = SysModuleResourcesClientService.SysModuleResourcesClientServiceFallbackFactory.class)
 public interface SysModuleResourcesClientService extends SysModuleResourcesRemoteService {
 
     @Component
-    class SysModuleResourcesClientServiceFallback implements SysModuleResourcesClientService{
+    class SysModuleResourcesClientServiceFallbackFactory implements FallbackFactory<SysModuleResourcesClientService> {
 
         @Override
-        public ResponseData<List<Map<String, Object>>> getModuleByUserId(String userId) {
-            return ResponseData.error("提供者出错了！");
+        public SysModuleResourcesClientService create(Throwable throwable) {
+            return new SysModuleResourcesClientService() {
+                @Override
+                public ResponseData<List<Map<String, Object>>> getModuleByUserId(String userId) {
+                    return ResponseData.error("提供者出错了！");
+                }
+            };
         }
     }
 }
--- dcy-business/admin-center/admin-center-client/src/main/java/com/dcy/client/SysUserInfoClientService.java
+++ dcy-business/admin-center/admin-center-client/src/main/java/com/dcy/client/SysUserInfoClientService.java
@@ -4,6 +4,7 @@ import com.dcy.api.InterfaceService;
 import com.dcy.api.model.SysUserInfo;
 import com.dcy.api.service.SysUserInfoRemoteService;
 import com.dcy.common.model.ResponseData;
+import feign.hystrix.FallbackFactory;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
 
@@ -14,21 +15,26 @@ import java.util.Set;
  * @Description: 用户表暴露远程接口+熔断
  * @Date: 2020/3/2 13:26
  */
-@FeignClient(name = InterfaceService.SERVICE_NAME, fallback = SysUserInfoClientService.SysUserInfoServiceFallback.class)
+@FeignClient(name = InterfaceService.SERVICE_NAME, fallbackFactory = SysUserInfoClientService.SysUserInfoServiceFallbackFactory.class)
 public interface SysUserInfoClientService extends SysUserInfoRemoteService {
 
+
     @Component
-    class SysUserInfoServiceFallback implements SysUserInfoClientService {
-
-
-        @Override
-        public ResponseData<SysUserInfo> getUserInfoByUsername(String username) {
-            return ResponseData.error("提供者出错了！");
-        }
+    class SysUserInfoServiceFallbackFactory implements FallbackFactory<SysUserInfoClientService> {
 
         @Override
-        public ResponseData<Set<String>> getAllPermissionByUserId(String userId) {
-            return ResponseData.error("提供者出错了！");
+        public SysUserInfoClientService create(Throwable throwable) {
+            return new SysUserInfoClientService() {
+                @Override
+                public ResponseData<SysUserInfo> getUserInfoByUsername(String username) {
+                    return ResponseData.error("提供者出错了！");
+                }
+
+                @Override
+                public ResponseData<Set<String>> getAllPermissionByUserId(String userId) {
+                    return ResponseData.error("提供者出错了！");
+                }
+            };
         }
     }
 }
